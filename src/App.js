532c83c3c5259ed1292fb6a51926ba5a0b263eee
jeremy lee <calijeremylee@gmail.com>
2018-10-11 15:53:12 -0700

    basic timer with clocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var clocks = {
+  10:'🕛',
+  9:'🕐',
+  8:'🕑',
+  7:'🕓',
+  6:'🕔',
+  5:'🕕',
+  4:'🕖',
+  3:'🕗',
+  2:'🕙',
+  1:'🕚',
+  0:'🕛'
+}
+// 🕛🕐🕑🕓🕔🕕🕖🕗🕙🕚🕛
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 10,
+      started: false,
+      id: 0
+    }
+    this.startTime = this.startTime.bind(this);
+    this.resetTime = this.resetTime.bind(this);
+  }
+
+  startTime() {
+    if (!this.state.started) {
+      const interval = setInterval(()=>{
+        let newTime = this.state.time - 1;
+        this.setState({ time: newTime }, ()=> {
+          if (this.state.time === 0) {
+            clearInterval(this.state.id);
+            let output = document.getElementById('output');
+            output.innerText = 'Finished!';
+          }
+        })
+      }, 1000)
+      this.setState({
+        id: interval,
+        started: true
+      })
+    }
+  }
+
+  resetTime() {
+    if (this.state.started) {
+      clearInterval(this.state.id);
+      this.setState({
+        time: 10,
+        started: false,
+        id: 0
+      })
+      let output = document.getElementById('output');
+      output.innerText = '';
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Timer </h1>
+        <div id="timer">
+          {clocks[this.state.time]}:{this.state.time}
+        </div>
+        <div id="buttons">
+          <button onClick={this.startTime}>Start Timer</button>
+          <button onClick={this.resetTime}>Reset</button>
+        </div>
+        <div id="output">
+        </div>
       </div>
     );
   }
